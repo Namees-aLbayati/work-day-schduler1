e2d9a13d755b4b0866feae43f96ba3f82e296994
Namees Albayati <nameesalbayati@Nameess-MacBook-Pro.local>
2022-01-24 13:47:28 -0600

    top

--- assets/script.js
+++ assets/script.js
@@ -17,16 +17,16 @@ $('#'+i).addClass("future");
     };
 
 $('#save').click(function (){
-    var eight=$("8").val();
+    var eight=$("#8").val();
 localStorage.setItem('8',eight);
 var nine=$("#9").val();
-localStorage.setItem('9',nine);
+localStorage.setItem('#9',nine);
 var ten=$('#10').val();
-localStorage.setItem('ten',ten);
+localStorage.setItem('10',ten);
 var eleven=$('#11').val();
-localStorage.setItem('eleven',eleven);
+localStorage.setItem('11',eleven);
 var twelv=$('#12').val();
-localStorage.setItem('twelv',twelv);
+localStorage.setItem('12',twelv);
 var th=$('#13').val();
 localStorage.setItem('13',th);
 var ft=$('#14').val();
@@ -48,7 +48,23 @@ alert('saved')
 window.onload = myfun();
 function myfun(){
 
-localStorage.getItem('8');
 $('#8').val(localStorage.getItem('8'));
+
+$('#8').val(localStorage.getItem('9'));
+$('#8').val(localStorage.getItem('10'));
+$('#8').val(localStorage.getItem('11'));
+
+$('#8').val(localStorage.getItem('12'));
+
+$('#8').val(localStorage.getItem('13'));
+$('#8').val(localStorage.getItem('14'));
+
+$('#8').val(localStorage.getItem('15'));
+
+$('#8').val(localStorage.getItem('16'));
+
+$('#8').val(localStorage.getItem('17'));
+$('#8').val(localStorage.getItem('18'));
+
 }
 
